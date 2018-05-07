99635a34d90263d00dc388e6788a856ecb6a6e67
lukasz91112 <226172@student.pwr.edu.pl>
2018-05-07 11:04:47 +0200

    Fix Makefile and project.c

--- project/project.c
+++ project/project.c
@@ -1,12 +1,13 @@
 #include "stdio.h"
 
 extern int square(int n);
+extern int calc(float x, float y, float r, int counter);
 
 int global_in_C= 777;
 
 int main()
 {
-    int i = square(25);
+    int i = calc(6, 5, 8, 4);
     printf("%d \n", i);
     return 0;
 }
